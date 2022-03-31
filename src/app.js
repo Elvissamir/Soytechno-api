06f771b95915ba5796ae4462ab7e2bc81e0dbcf3
Elvis <elvissamir18@gmail.com>
2022-03-31 18:50:22 -0400

    Add middleware to app on server startup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const routes = require('./startup/routes')
+const middleware = require('./startup/middleware')
 const app = express()
 
 routes(app)
+middleware(app)
 
 module.exports = app
--- src/startup/middleware.js
+++ src/startup/middleware.js
@@ -1,3 +1,11 @@
-module.exports = function () {
-    
+const cors = require('cors')
+const express = require('express')
+const helmet = require('helmet')
+const compression = require('compression')
+
+module.exports = function (app) {
+    app.use(express.json())
+    app.use(helmet())
+    app.use(cors())
+    app.use(compression())
 }
