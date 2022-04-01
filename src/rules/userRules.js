813dd34fe84a30c28d1a31d6ec790ea34a1a929b
Elvis <elvissamir18@gmail.com>
2022-04-01 18:53:07 -0400

    Add User Rules in rules

---
+++ src/rules/userRules.js
@@ -0,0 +1,13 @@
+const userRules = {
+    fnameMaxChars: 15,
+    fnameMinChars: 2,
+    lnameMaxChars: 15,
+    lnameMinChars: 2,
+    ciMaxChars: 10,
+    ciMinChars: 7,
+    emailMaxChars: 255,
+    passwordMaxChars: 20,
+    passwordMinChars: 6
+}
+
+module.exports = userRules
