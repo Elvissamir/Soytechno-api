34e8fde9a3b4d51a734add15b61f662fc8b30e80
Elvis <elvissamir18@gmail.com>
2022-04-01 19:48:41 -0400

    Add users Controller

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,9 +1,12 @@
-const { testRoute } = require('../endpoints/index')
+const { testRoute, userRoute } = require('../endpoints/index')
+const userController = require('../controllers/users')
 const testController = require('../controllers/state')
 const errorController = require('../middleware/error')
 
 module.exports = function (app) {
     if (process.env.APP_ENV != 'production')
         app.use(testRoute, testController)
+    
+    app.use(userRoute, userController)
     app.use(errorController)
 }
