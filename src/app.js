9af8b3d75db1c1704bf3b4c3c00025e873309d7c
Elvis <elvissamir18@gmail.com>
2022-04-01 11:39:10 -0400

    Create .env and .env.example Add dotenv.config() in app.js for loading environment variables into process.env

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const routes = require('./startup/routes')
 const middleware = require('./startup/middleware')
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = express()
 
 middleware(app)
--- src/startup/routes.js
+++ src/startup/routes.js
@@ -2,6 +2,9 @@ const testController = require('../controllers/state')
 const errorController = require('../middleware/error')
 
 module.exports = function (app) {
-    app.use('/api/test', testController)
+    if (!process.env.APP_ENV === 'production')
+        app.use('/api/test', testController)
+
+    console.log(process.env.APP_ENV)
     app.use(errorController)
 }
