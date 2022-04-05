7e3efa832df59cf8365d591a2ce9f0d762ab7b31
Elvis <elvissamir18@gmail.com>
2022-04-05 11:18:59 -0400

    Add validateLogin interactor

--- src/controllers/login/login.controller.js
+++ src/controllers/login/login.controller.js
@@ -1,12 +1,11 @@
 const router = require('express').Router()
+const { validateLogin } = require('../../interactors/index')
 
 router.post('/', async (req, res) => {
+    // return if invalid login
+    const { error, token } = await validateLogin(req.body)
 
-    // validate data
-    // if data is valid 
-    // create token
-    // return the token
-
+    // return token if valid login
     return res.send("Hello")
 })
 
--- src/interactors/index.js
+++ src/interactors/index.js
@@ -1,12 +1,15 @@
 const registerUserInteractor = require('./registerUser')
 const validateUserInteractor = require('./validateUser')
+const validateLoginInteractor = require('./validateLogin')
 
 const UserMongo = require('../dataSources/User')
 
+const validateLogin = validateLoginInteractor(UserMongo)
 const validateUser = validateUserInteractor(UserMongo)
 const registerUser = registerUserInteractor(UserMongo)
 
 module.exports = {
     registerUser,
-    validateUser
+    validateUser,
+    validateLogin,
 }
