043a257819c93a7d190f12ffc8a1ac30cd93be23
Elvis <elvissamir18@gmail.com>
2022-04-13 18:30:31 -0400

    Add auth middleware test

--- src/middleware/auth.middleware.test.js
+++ src/middleware/auth.middleware.test.js
@@ -1,5 +1,52 @@
+const validUser = require('../utils/test-utils/validUserData')
+const User = require('../dataSources/User')
+const userData = require('../utils/test-utils/validUserData')
+
 describe('Auth Middleware', () => {
-    it('Should allow the request if the auth data is valid', () => {
+    let user
+    let token
+    let req
+    let res
+    let next = jest.fn()
+
+    beforeEach(() => {
+        userData = validUser
+        userData['password'] = User.hashPassword(validUser.password)
+
+        user = new User(userData)
+
+        token = user.generateAuthToken()
+
+        req = {
+            params: {}, 
+            body: {},
+            headers: {
+                'x-auth-token': token,
+                'accsess-control-expose-headers': 'x-auth-token'
+            }
+        }
+
+        res = {
+            data: null, 
+            code: null, 
+
+            status (status) {
+                this.code = status
+                return this.code
+            },
+
+            send (payload) {
+                this.data = payload
+            }
+        }
+
+        next.mockClear()
+    })
+
+    it('Should allow the request if the token is valid', async () => {
         
+        authMiddleware(req, res, next)
+
+        expect(next).toBeCalledTimes(1)
     })
 })
