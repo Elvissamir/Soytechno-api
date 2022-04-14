9bf2e3e1fecb2f807898335cfa74d0d5dbdc9645
Elvis <elvissamir18@gmail.com>
2022-04-14 14:47:10 -0400

    Remove validateToken Interactor and Test. Move logic into authToken validator

--- src/interactors/validateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-const validateAuthToken = require('../validators/authToken.validator')
-
-module.exports = (UserDataSource) => (token) => {
-    // validate jwt token
-    // if invalid return false
-    // if valid return true
-    return true
-}
