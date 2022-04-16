8041fab628d750d23ebdc38dbff98f0688e6c6b1
Elvis <elvissamir18@gmail.com>
2022-04-16 18:25:15 -0400

    Add new tests in Products controller: Not Authorized, Invalid data, Invalid token, Unauthenticated

--- src/controllers/products/products.controller.post.test.js
+++ src/controllers/products/products.controller.post.test.js
@@ -9,14 +9,26 @@ describe('Products Controller / POST', () => {
     })
 
     it('Should return 401 if the user is not authenticated', async () => {
-        
+        // send post request without auth token
+        // expect status 401
+        // expect error message Access denied. No token provided
+    })
+
+    it('should return 400 if the token is invalid', async () => {
+        // send post request with invalid token
+        // expect status 400
+        // expect error message Invalid token
     })
 
     it('Should return error status code if the user is not authorized', async () => {
-
+        // send post request with token from non admin user
+        // expect status 4XX
+        // expect error message FORBIDDEN
     })
 
     it('Should return 400 if the provided data is invalid', async () => {
-        
+        // send post request with invalid data
+        // expect status 400 
+        // expect validation error message
     })
 })
