5d87f7f2f4c51f9160e0cb15d50f6918dcc8ecce
Elvis <elvissamir18@gmail.com>
2022-04-16 18:19:21 -0400

    Add tests in Products controller post

--- src/controllers/products/products.controller.post.test.js
+++ src/controllers/products/products.controller.post.test.js
@@ -1,3 +1,22 @@
 describe('Products Controller / POST', () => {
-    
+
+    it('Should create a new post with given data', async () => {
+        // expect the database has 0 products
+        // send post request with data
+        // expect the database has 1 product
+        // expect response to be 200
+        // expect response to have the created product
+    })
+
+    it('Should return 401 if the user is not authenticated', async () => {
+        
+    })
+
+    it('Should return error status code if the user is not authorized', async () => {
+
+    })
+
+    it('Should return 400 if the provided data is invalid', async () => {
+        
+    })
 })
