ba2bf9221dcb4e4353cb3037915157220621f99c
Elvis <elvissamir18@gmail.com>
2022-04-17 18:16:14 -0400

    Add test assertions to Should create a new post with given data test in Products controller

--- src/controllers/products/products.controller.post.test.js
+++ src/controllers/products/products.controller.post.test.js
@@ -1,19 +1,39 @@
 const app = require('../../app')
 const request = require('supertest')
+const { productEndpoint } = require('../../endpoints/index')
 const dbTestHandler = require('../../utils/test-utils/dbTestHandler')
 
 describe('Products Controller / POST', () => {
 
-    const sendPostRequest = () => {
-        return request(app).post()
+    const sendPostRequest = (data) => {
+        return request(app).post(productEndpoint).send(data)
     }
 
     it('Should create a new post with given data', async () => {
         // expect the database has 0 products
+        const productsCountBefore = Product.count()
+        expect(productsCountBefore).toBe(0)
+
         // send post request with data
+        const productData = {}
+        const response = await sendPostRequest(productData)
+
         // expect the database has 1 product
+        const productsCountAfter = Product.count()
+        expect(productsCountAfter).toBe(1)
+
         // expect response to be 200
+        expect(response.status).toBe(200)
+
         // expect response to have the created product
+        expect(response.body).toHaveProperty('_id')
+        expect(response.body).toHaveProperty('title')
+        expect(response.body).toHaveProperty('price')
+        expect(response.body).toHaveProperty('inStock')
+        expect(response.body).toHaveProperty('categories')
+        expect(response.body).toHaveProperty('description')
+        expect(response.body).toHaveProperty('discount')
+        expect(response.body).toHaveProperty('rating')
     })
 
     it('Should return 401 if the user is not authenticated', async () => {
