7faaa936cebda16732dc1f3377a637c39845c21f
Elvis <elvissamir18@gmail.com>
2022-04-17 18:37:47 -0400

    Add Product Schema for mongoose model

--- src/dataSources/Product.js
+++ src/dataSources/Product.js
@@ -1,7 +1,43 @@
+const rules = require('../rules/productRules')
 const { Schema, Model } = require('mongoose')
 
 const productSchema = new Schema({
-    
+    title: {
+        type: String,
+        minlength: rules.titleMinChars,
+        maxlength: rules.titleMaxChars,
+        trim: true,
+        required: true
+    },
+    inStock: {
+        type: Number,
+        min: rules.inStockMin,
+        max: rules.inStockMax,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        minlength: rules.descriptionMinChars,
+        maxlength: rules.descriptionMaxChars,
+        trim: true,
+        required: true
+    },
+    discount: {
+        type: Number,
+        min: rules.discountMin,
+        max: rules.discountMax,
+        default: 0
+    }, 
+    rating: {
+        type: Number,
+        min: rules.ratingMin,
+        max: rules.ratingMax,
+        default: null
+    }
 })
 
 module.exports = Product
