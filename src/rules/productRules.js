801785dcff4a43fd1c35844660c98f7642682930
Elvis <elvissamir18@gmail.com>
2022-04-17 18:29:05 -0400

    Add Product rules

---
+++ src/rules/productRules.js
@@ -0,0 +1,14 @@
+const rules = {
+    titleMaxChars: 50,
+    titleMinChars: 10,
+    inStockMin: 0,
+    inStockMax: 150,
+    descriptionMaxChars: 250,
+    descriptionMinChars: 20,
+    discountMin: 0,
+    discountMax: 90,
+    ratingMin: 0,
+    ratingMax: 5
+}
+
+module.exports = rules
