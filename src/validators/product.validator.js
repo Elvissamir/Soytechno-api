2fde8f823d382a0916be8a8c6389d73888688780
Elvis <elvissamir18@gmail.com>
2022-04-19 16:00:01 -0400

    Add Product title length should be less or equal to the max allowed

--- src/validators/product.validator.js
+++ src/validators/product.validator.js
@@ -2,7 +2,7 @@ const rules = require('../rules/productRules')
 const Joi = require('joi')
 
 const schema = Joi.object({
-    title: Joi.string().required().label('Title'),
+    title: Joi.string().max(rules.titleMaxChars).required().label('Title'),
     price: Joi.any(),
     inStock: Joi.any(),
     description: Joi.any(),
--- src/validators/product.validator.test.js
+++ src/validators/product.validator.test.js
@@ -1,3 +1,4 @@
+const rules = require('../rules/productRules')
 const productValidator = require('./product.validator')
 
 describe('Product Validator', () => {
@@ -26,5 +27,14 @@ describe('Product Validator', () => {
         expect(result).toHaveProperty('error')
         expect(result.error.details[0].message.includes('Title')).toBe(true)
         expect(result.error.details[0].message.includes('string')).toBe(true) 
+    })
+
+    it ('Should validate the title has less than or equal amount of chars to the max', () => {
+        data['title'] = new Array(rules.titleMaxChars + 1).fill('a').join('')
+        const result = productValidator(data)
+
+        expect(result).toHaveProperty('error')
+        expect(result.error.details[0].message.includes('Title')).toBe(true)
+        expect(result.error.details[0].message.includes('less than')).toBe(true) 
     })
 })
