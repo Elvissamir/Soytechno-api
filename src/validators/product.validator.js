0c90d4308574005cd4564dc71b9ec430e0c71958
Elvis <elvissamir18@gmail.com>
2022-04-19 16:18:50 -0400

    Add tests for Product description: required, is a string, has less than the max amount of chars, has at least the min amount of chars

--- src/validators/product.validator.js
+++ src/validators/product.validator.js
@@ -2,7 +2,7 @@ const rules = require('../rules/productRules')
 const Joi = require('joi')
 
 const schema = Joi.object({
-    title: Joi.string().max(rules.titleMaxChars).required().label('Title'),
+    title: Joi.string().min(rules.titleMinChars).max(rules.titleMaxChars).required().label('Title'),
     price: Joi.any(),
     inStock: Joi.any(),
     description: Joi.any(),
--- src/validators/product.validator.test.js
+++ src/validators/product.validator.test.js
@@ -37,4 +37,51 @@ describe('Product Validator', () => {
         expect(result.error.details[0].message.includes('Title')).toBe(true)
         expect(result.error.details[0].message.includes('less than')).toBe(true) 
     })
+
+    it ('Should validate the title has more than or equal amount of chars to the min', () => {
+        data['title'] = new Array(rules.titleMinChars - 1).fill('a').join('')
+        const result = productValidator(data)
+
+        expect(result).toHaveProperty('error')
+        expect(result.error.details[0].message.includes('Title')).toBe(true)
+        expect(result.error.details[0].message.includes('at least')).toBe(true) 
+    }) 
+
+    it ('Should validate the description is required', () => {
+        delete data.description
+        const result = productValidator(data)
+
+        expect(result).toHaveProperty('error')
+        expect(result.error.details[0].message.includes('Description')).toBe(true)
+        expect(result.error.details[0].message.includes('required')).toBe(true) 
+    })
+
+    it ('Should validate the title is a string', () => {
+        data['description'] = 100
+        const result = productValidator(data)
+
+        expect(result).toHaveProperty('error')
+        expect(result.error.details[0].message.includes('Description')).toBe(true)
+        expect(result.error.details[0].message.includes('string')).toBe(true) 
+    })
+
+    it ('Should validate the title has less than or equal amount of chars to the max', () => {
+        data['title'] = new Array(rules.descriptionMaxChars + 1).fill('a').join('')
+        const result = productValidator(data)
+
+        expect(result).toHaveProperty('error')
+        expect(result.error.details[0].message.includes('Description')).toBe(true)
+        expect(result.error.details[0].message.includes('less than')).toBe(true) 
+    })
+
+    it ('Should validate the title has more than or equal amount of chars to the min', () => {
+        data['title'] = new Array(rules.descriptionMinChars - 1).fill('a').join('')
+        const result = productValidator(data)
+
+        expect(result).toHaveProperty('error')
+        expect(result.error.details[0].message.includes('Description')).toBe(true)
+        expect(result.error.details[0].message.includes('at least')).toBe(true) 
+    }) 
+
+
 })
